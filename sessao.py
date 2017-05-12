225f64ea5dd073481798a6696615939ebec5f13e
AlmeidaTe <terezaalnobre@gmail.com>
2017-05-11 21:23:29 -0300

    Update sessao.py

--- sessao.py
+++ sessao.py
@@ -1,12 +1,12 @@
-sessoes = []
+
 import sala
 import filme
 
-def criar_sessao(cod_sessao,cod_filme,cod_sala,horario,capacidade):
+def sessao(cod_sessao,cod_filme,cod_sala,horario,capacidade):
     sessao = [cod_sessao,cod_filme, cod_sala, horario,capacidade]
     sessoes.append(sessao)
     
-def alterar_sessoes(cod_sessao, cod_filme, cod_sala, horario, capacidade):
+def sessoes(cod_sessao, cod_filme, cod_sala, horario, capacidade):
     for a in sessoes:
         if a[0] == cod_sessao:
             a[1] = cod_filme
@@ -16,23 +16,23 @@ def alterar_sessoes(cod_sessao, cod_filme, cod_sala, horario, capacidade):
             return True
         return False
     
-def buscar_sessao(cod_sessao):
+def sessao(cod_sessao):
     for s in sessoes:
         if s[0] == cod_sessao:
             return s
         
-def verificar_disponibilidade(cod_sessao):
+def disponibilidade(cod_sessao):
     s= buscar_sessao(cod_sessao)
     return s
     
-def excluir_sessao(cod_sessao):
+def sessao(cod_sessao):
     for s in sessoes:
         if s[0] ==cod_sessao:
             sessoes.remove(s)
             return True
         return False
-def listar_sessao():
+def sessao():
     return sessoes
-def remover_todas_as_sessoes():
-    global sessoes
+def remover_sessoes():
+    
     sessoes=[]
