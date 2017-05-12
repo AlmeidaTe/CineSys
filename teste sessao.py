2043ded43fa2d0d27693b9a1a8bfbc814098c25a
AlmeidaTe <terezaalnobre@gmail.com>
2017-05-11 21:16:24 -0300

    Update teste sessao.py

--- teste sessao.py
+++ teste sessao.py
@@ -1,61 +1,106 @@
-import unittest
 import sessao
+import sala
+import filme
 
-class TestSala(unittest.TestCase):
+def imprimir_sessao(sessoes):
+    print(sessoes)
+    print('Cod. Sessão: ',sessoes[0])
+    print('Cod. Filme: ', sessoes[1])
+    print('Cod sala: ',sessoes[2])
+    print('Horário: ',sessoes[3])
+    print('Capacidade: ',sessoes[4])
+    
+def criar():
+    cod_sessao = int(input('Digite o código da sessão: '))
+    cod_filme = int(input('Digite o código do filme: '))
+    cod_sala = int(input('Digite o código da sala: '))
+    horario = int(input('Digite o horário: '))
+    capacidade = int(input('Digite a capacidade da sessão :'))
+    sessao.criar_sessao(cod_sessao,cod_filme,cod_sala,horario,capacidade)
 
-    def setUp(self):
-        sessao.remover_todas_as_sessoes()
+def buscar():
+    cod_sessao= int(input('Buscar sessão pelo código da sessão: '))
+    o = sessao.buscar_sessao(cod_sessao)
+    if o==None:
+        print('Sessão não encontrada! ')
+    else:
+        imprimir_sessao(o)
 
-    def test_criar_sessao(self):
-        sessao.criar_sessao(36,65,11,9,23)
-        sessoes = sessao.listar_sessao()
-        self.assertEqual(1,len(sessoes))
-        s=sessoes[0]
-        self.assertEqual(36,s[0])
-        self.assertEqual(65,s[1])
-        self.assertEqual(11,s[2])
-        self.assertEqual(9,s[3])
-        self.assertEqual(23,s[4])
+def listar():
+    print(' Listar sessões \n')
+    sessoes = sessao.listar_sessao()
+    for s in sessoes:
+        imprimir_sessao(s)
+
+def excluir():
+    print('\n Excluir sessão')
+    cod_sessao = int(input('Cod. Sessao: '))
+    s = sessao.excluir_sessao(cod_sessao)
+    if (s == False):
+        print ("Sessao não encontrada")
+    else:
+        print ("Sessão excluida")
+    
+def alterar():
+    print('\n Alterar sessão')
+    cod_sessao = int(input('Cod. Sessão: '))
+    s = sessao.listar_sessao()
+    if (s == False):
+        print ("Sessão não encontrada")
+    else:
+        cod_filme = int(input('Digite o novo código do filme: '))
+        cod_sala = int(input('Digite o novo código da sala: '))
+        horario = int(input('Digite o novo horário: '))
+        capacidade = int(input('Digite a nova capacidade da sessão :'))
+        s = sessao.buscar_sessao(cod_sessao)
+        s[1] = cod_filme
+        s[2] = cod_sala
+        s[3] = horario
+        s[4] = capacidade
+        ss= s[1] and s[2] and s[3] and s[4]
+        sessao.alterar_sessoes(cod_sessao, cod_filme, cod_sala, horario, capacidade)
+        return ss
+
+def excluirTodas():
+    sessao.remover_todas_as_sessoes()
+
+def verificar():
+    cod_sessao = int(input('Digite o cód. sessão: '))
+    print('Capacidade da sessão é de :',sessao.verificar_disponibilidade(cod_sessao), ' pessoas.')
         
-    def test_buscar_sessao(self):
-        sessao.criar_sessao(36,65,11,9,23)
-        sessao.criar_sessao(3,5,1,2,65)
+def mostrar():
+    run_sessao = True
+    menu = ("\n----------------\n"+
+             "(1) Criar sessão \n" +
+             "(2) Listar sessão \n" +
+             "(3) Buscar sessão por Cód. sessão\n" +
+             "(4) Excluir sessão \n" +
+             "(5) Alterar sessão \n"+
+             "(6) Remover TODAS as sessões \n"+
+             "(7) Verificar disponibilidade da sessao \n" + 
+             "(0) Voltar\n"+
+            "----------------")
+    
+    while(run_sessao):
+        print (menu)
+        op = int(input("Digite sua escolha: "))
 
-        s=sessao.buscar_sessao(3)
+        if op == 1:
+            criar()
+        elif op == 2:
+            listar()
+        elif op == 3:       
+            buscar()
+        elif op == 4:
+            excluir()
+        elif op == 5:
+            alterar()
+        elif op == 6:
+            excluirTodas()
+        elif op ==7:
+            verificar()
+        elif op == 0:
+            sessao = False
 
-        self.assertEqual(3,s[0])
-        self.assertEqual(5,s[1])
-        self.assertEqual(1,s[2])
-        self.assertEqual(2,s[3])
-        self.assertEqual(65,s[4])
-       
-    def test_excluir_sessao(self):
-        sessao.criar_sessao(36,65,11,9,23)
-        sessao.criar_sessao(3,5,1,2,65)
-        sessao.excluir_sessao(36)
-        s = sessao.buscar_sessao(36)
-        self.assertIsNone(s)
-
-    def test_alterar_sessao(self):
-        sessao.criar_sessao(36,65,11,9,23)
-        sessao.criar_sessao(3,69,11,12,65)
-
-        s=sessao.buscar_sessao(3)
-        s[1] = 69
-        s[2] = 11
-        s[3] = 12
-        s[4] = 65
-        self.assertEqual(3,s[0])
-        self.assertEqual(69,s[1])
-        self.assertEqual(11,s[2])
-        self.assertEqual(12,s[3])
-        self.assertEqual(65,s[4])
-
-    def test_verificar_disponibilidade(self):
-        sessao.criar_sessao(36,65,11,9,23)
-        sessao.criar_sessao(3,5,1,2,65)
-        s = sessao.verificar_disponibilidade(36)
-        self.assertEqual(23,s[4])
-
-if __name__ =='__main__':
-    unittest.main(exit=False)
+if __name__ == "__main__":
+    menu_mostrar()
