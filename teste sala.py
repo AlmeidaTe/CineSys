2e682b81e7d65702e17ef93ea28b527fc7c1d7ce
AlmeidaTe <terezaalnobre@gmail.com>
2017-05-14 19:05:31 -0300

    Alteração 11
    Colocar mais testes nos módulos alterar sala, pois assim pode se ter a certeza que a função está fazendo o devido trabalho.

--- teste sala.py
+++ teste sala.py
@@ -7,15 +7,14 @@ class TestSala(unittest.TestCase):
     def setUp(self):
         sala.remover_todas_as_salas()
     
-    def _criar_sala(self):
+    def criar_sala(self):
         sala.criar_sala(11,30)
         salas = sala.listar_salas()
         self.assertEqual(1,len(salas))
         s=salas[0]
         self.assertEqual(11,s[0])
-        
 
-    def _obter_sala(self):
+    def obter_sala(self):
         sala.criar_sala(11,30)
         sala.criar_sala(3,58)
         
@@ -23,21 +22,24 @@ class TestSala(unittest.TestCase):
         
         self.assertEqual(11,s[0])
         
-    def _excluir_sala(self):
+    def excluir_sala(self):
         sala.criar_sala(11,30)
         sala.criar_sala(3,58)
         sala.excluir_sala(3)
         s = sala.obter_sala(3)
         self.assertIsNone(s)
         
-    def _alterar_sala(self):
+    def alterar_sala(self):
         sala.criar_sala(11,30)
         sala.criar_sala(3,58)
         s = sala.obter_sala(11)
         salas = sala.listar_salas()
         s[1] = 60
+        self.assertEqual(11,s[0])
         self.assertEqual(60,s[1])
 
 if __name__ =='__main__':
     unittest.main(exit=False)
+
+
 
